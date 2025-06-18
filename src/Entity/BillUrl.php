64e19ab21dbed680ff4f1f65d8c4ac3d4f431efe
github-ci <you@example.com>
2025-06-18 20:17:23 +0800

    remove Attribute

--- src/Entity/BillUrl.php
+++ src/Entity/BillUrl.php
@@ -8,43 +8,32 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
 use Tourze\DoctrineTimestampBundle\Traits\TimestampableAware;
-use Tourze\EasyAdmin\Attribute\Column\ExportColumn;
-use Tourze\EasyAdmin\Attribute\Column\ListColumn;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '支付宝账单')]
 #[ORM\Entity(repositoryClass: BillUrlRepository::class)]
 #[ORM\Table(name: 'alipay_trade_bill_url', options: ['comment' => '账单URL'])]
 #[ORM\UniqueConstraint(name: 'alipay_trade_bill_url_idx_uniq', columns: ['account_id', 'type', 'date'])]
 class BillUrl implements \Stringable
 {
-    #[ListColumn(order: -1)]
-    #[ExportColumn]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: Types::INTEGER, options: ['comment' => 'ID'])]
     private ?int $id = 0;
 
-    #[ListColumn(title: '支付宝应用')]
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
     private Account $account;
 
-    #[ListColumn]
     #[IndexColumn]
     #[ORM\Column(length: 40, enumType: BillType::class, options: ['comment' => '账单类型'])]
     private BillType $type = BillType::trade;
 
-    #[ListColumn]
     #[IndexColumn]
     #[ORM\Column(type: Types::DATE_IMMUTABLE, options: ['comment' => '日期'])]
     private \DateTimeInterface $date;
 
-    #[ListColumn]
     #[ORM\Column(length: 1000, options: ['comment' => '原始下载地址'])]
     private string $downloadUrl;
 
-    #[ListColumn]
     #[ORM\Column(length: 1000, nullable: true, options: ['comment' => '本地下载地址'])]
     private ?string $localFile = null;
 
